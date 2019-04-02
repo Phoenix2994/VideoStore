5f71b863ee851ae4c42113ee279b1861dbe64823
Bianco Filippo <filippo.bianco@firenze.eng.it>
2019-04-02 15:04:42 +0200

    adding movie-details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'movies/:title', component: MovieDetailComponent },
+  { path: 'dashboard/:title', component: MovieDetailComponent },
 ];
 
 @NgModule({
--- src/app/movies-list.ts
+++ src/app/movies-list.ts
@@ -1,20 +1,26 @@
+
 export class Movie {
-    title: string;
-    year: number;
-    genre: string;
-    movieDirector: string;
-    duration: number;
-  }
+  title: string;
+  year: number;
+  genre: string;
+  movieDirector: string;
+  duration: number;
+  // tslint:disable-next-line:max-line-length
+  plot = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus justo justo, in convallis elit aliquet sit amet. Nullam et bibendum tortor, in volutpat ante. Morbi consectetur feugiat lectus, vel aliquet orci pharetra consectetur. Morbi hendrerit ligula id quam lobortis iaculis. Praesent in nunc ac lacus finibus suscipit eu quis ligula. Pellentesque nec pellentesque ex, in mollis libero. Morbi quis mollis arcu, nec sagittis purus. Aenean quam tellus, efficitur sed bibendum posuere, varius et neque. Maecenas porttitor sit amet purus et ultrices. Nullam iaculis luctus luctus. Maecenas at nisi efficitur, fermentum mauris nec, semper magna. Nulla dictum metus vitae nisi maximus sodales ac in orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc molestie vehicula tincidunt. Ut libero nunc, fermentum eget volutpat sed, imperdiet vitae sapien.Mauris blandit a sapien quis accumsan. Curabitur consectetur a tellus vitae molestie. Proin vitae sagittis purus, nec consequat ligula. Aenean feugiat ipsum sollicitudin purus euismod, ac faucibus nisl ornare. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue tincidunt pretium. Phasellus interdum, augue eget vehicula rhoncus, lacus risus blandit neque, nec tincidunt odio enim eu erat. Mauris enim mauris, aliquam eu arcu ut, aliquam eleifend dolor. Donec laoreet augue eget risus dignissim, sed pulvinar urna congue.Aenean finibus magna vel ullamcorper efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi viverra est vitae massa ullamcorper, id mollis tortor laoreet. Aliquam sit amet mi cursus, suscipit diam sagittis, vehicula nisl. Nunc consectetur id diam vel dictum. Ut cursus non dolor ut tempor. Vivamus a cursus turpis. Integer in cursus diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse ultricies tellus ac lobortis pretium. Vivamus nec pellentesque arcu. Aliquam erat volutpat. Nulla commodo sem sed mi suscipit fermentum.';
+}
+
+// tslint:disable-next-line:max-line-length
+export const plot = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus justo justo, in convallis elit aliquet sit amet. Nullam et bibendum tortor, in volutpat ante. Morbi consectetur feugiat lectus, vel aliquet orci pharetra consectetur. Morbi hendrerit ligula id quam lobortis iaculis. Praesent in nunc ac lacus finibus suscipit eu quis ligula. Pellentesque nec pellentesque ex, in mollis libero. Morbi quis mollis arcu, nec sagittis purus. Aenean quam tellus, efficitur sed bibendum posuere, varius et neque. Maecenas porttitor sit amet purus et ultrices. Nullam iaculis luctus luctus. Maecenas at nisi efficitur, fermentum mauris nec, semper magna. Nulla dictum metus vitae nisi maximus sodales ac in orci. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc molestie vehicula tincidunt. Ut libero nunc, fermentum eget volutpat sed, imperdiet vitae sapien.Mauris blandit a sapien quis accumsan. Curabitur consectetur a tellus vitae molestie. Proin vitae sagittis purus, nec consequat ligula. Aenean feugiat ipsum sollicitudin purus euismod, ac faucibus nisl ornare. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent congue tincidunt pretium. Phasellus interdum, augue eget vehicula rhoncus, lacus risus blandit neque, nec tincidunt odio enim eu erat. Mauris enim mauris, aliquam eu arcu ut, aliquam eleifend dolor. Donec laoreet augue eget risus dignissim, sed pulvinar urna congue.Aenean finibus magna vel ullamcorper efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Morbi viverra est vitae massa ullamcorper, id mollis tortor laoreet. Aliquam sit amet mi cursus, suscipit diam sagittis, vehicula nisl. Nunc consectetur id diam vel dictum. Ut cursus non dolor ut tempor. Vivamus a cursus turpis. Integer in cursus diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse ultricies tellus ac lobortis pretium. Vivamus nec pellentesque arcu. Aliquam erat volutpat. Nulla commodo sem sed mi suscipit fermentum.';
 
 export const MOVIES: Movie[] = [
-    {title: 'Rec', year: 2007, genre: 'Horror', movieDirector: 'Jaume Balagueró', duration: 78 },
-    { title: 'Seven', year: 1995, genre: 'Thriller', movieDirector: 'David Fincher', duration: 128 },
-    { title: 'Titanic', year: 1997, genre: 'Romantico', movieDirector: 'James Cameron', duration: 195 },
-    { title: 'I Simpson ', year: 2007, genre: 'Commedia', movieDirector: 'David Silverman', duration: 87 },
-    { title: 'Jurassic Park', year: 1997, genre: 'Avventura', movieDirector: 'Steven Spielberg', duration: 127 },
-    { title: 'Non ci resta che piangere', year: 1984, genre: 'Commedia', movieDirector: 'Massimo Troisi', duration: 145 },
-    { title: 'Inception', year: 2010, genre: 'Thriller', movieDirector: 'Christopher Nolan', duration: 148 }
-  ];
+  { title: 'Rec', year: 2007, genre: 'Horror', movieDirector: 'Jaume Balagueró', duration: 78, plot },
+  { title: 'Seven', year: 1995, genre: 'Thriller', movieDirector: 'David Fincher', duration: 128, plot},
+  { title: 'Titanic', year: 1997, genre: 'Romantico', movieDirector: 'James Cameron', duration: 195, plot},
+  { title: 'I Simpson ', year: 2007, genre: 'Commedia', movieDirector: 'David Silverman', duration: 87, plot },
+  { title: 'Jurassic Park', year: 1997, genre: 'Avventura', movieDirector: 'Steven Spielberg', duration: 127, plot},
+  { title: 'Non ci resta che piangere', year: 1984, genre: 'Commedia', movieDirector: 'Massimo Troisi', duration: 145, plot},
+  { title: 'Inception', year: 2010, genre: 'Thriller', movieDirector: 'Christopher Nolan', duration: 148, plot}
+];
 
 export const GENRES: string[] = [
   'Avventura', 'Commedia', 'Horror', 'Thriller', 'Romantico', 'Qualsiasi'
